848dd0447424701fda4b603205375bf3397fd551
tejasmahalinga <tejasmahalinga@gmail.com>
2022-06-08 22:47:48 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {ScrollView, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <ScrollView>
+      <View>
+        <Text>Calculator app</Text>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default App;
